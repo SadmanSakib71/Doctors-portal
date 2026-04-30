ac57156981358c67a24f6023188969cdf60e6289
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-30 17:10:55 +0600

    text change in userRoute

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/:clerkId", async (req, res) => {
   try {
     const user = await User.find({ clerkId: req.params.clerkId });
-    res.status(200).json({ data: user, message: "User get successfully" });
+    res.status(200).json({ data: user, message: "User got successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
